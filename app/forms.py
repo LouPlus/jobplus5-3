d1bb1248bf9ebcccd01f24597cc183deabe49d2e
ZFane <justonezf@gmail.com>
2018-04-13 14:57:52 +0800

    编写登录与注册

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, ValidationError
+from wtforms.validators import DataRequired, Email, Length, ValidationError,EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -16,3 +16,20 @@ class LoginForm(FlaskForm):
     #     user=User.query.filter_by(email=self.email.data)
     #     if user and not user.check_password:
     #         raise ValidationError('登录失败')
+
+class RegisterForm(FlaskForm):
+    username=StringField('用户名',validators=[DataRequired('注册失败'),Length(min=6)])
+    email=StringField('邮箱',validators=[DataRequired('注册失败'),Email()])
+    password=PasswordField('密码',validators=[DataRequired('注册失败'),Length(min=6,max=36)])
+    re_password=PasswordField('重复密码',validators=[DataRequired('注册失败'),Length(min=6,max=36),EqualTo('password')])
+    submit=SubmitField('提交')
+
+    def create_user(self):
+        pass
+
+
+class CompanyProfile(FlaskForm):
+    pass
+class UserProfile(FlaskForm):
+    pass
+
--- app/main/home.py
+++ app/main/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, flash, url_for, abort
 from flask_login import login_user, login_required, logout_user
 
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 
 home=Blueprint('home',__name__,url_prefix='/')
 
@@ -13,26 +13,40 @@ def index():
 def login():
     form=LoginForm()
     if form.validate_on_submit():
+        #做角色判断，登录的 用户是 企业用户还是个人用户
         # user=User.query.filter_by(email=form.email.data).first()
         # login_user(user,True)
         flash('登录成功','success')
         return redirect(url_for('.index'))
-    else:
-        flash('登录失败，请重试','danger')
     return render_template("home/login.html",form=form)
+
 @home.route('register/<string:type>',methods=['post','get'])
 def register(type):
+    form=RegisterForm()
+    if form.validate_on_submit():
+        flash('注册成功','success')
+        return redirect(url_for('.index'))
     if type=='user':
         #用户注册
-        pass
+        form_title='用户注册'
+        form.username.label.text='用户名'
     elif type=='company':
         #企业注册
-        pass
-    else:
-        abort(404)
+        form_title='企业注册'
+        form.username.label.text='企业名称'
+    return render_template('home/register.html',form=form,type=type,form_title=form_title)
 
 @home.route('logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('.index'))
+
+@home.route('job')
+def job_list():
+    pass
+
+@home.route('company')
+def company_list():
+    pass
+
